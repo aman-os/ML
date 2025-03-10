13427caffec4fbfa3e9f5eec1a415e628f2e50ba
Aman Chandran <amanchandran@aol.com>
2025-03-10 12:24:16 +0530

    Update PlacementStatus.py

--- PlacementStatus.py
+++ PlacementStatus.py
@@ -3,7 +3,12 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 # Load dataset
-df ='placedata v2.0 synthetic.csv'
+uploaded_file = st.file_uploader("Upload CSV file", type="csv")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())  # Display first few rows
+
 
 
 # Function to load the saved model and label encoders
